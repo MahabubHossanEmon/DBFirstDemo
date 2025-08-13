f539e9131dbe5a5906a2b545d2b4767d67d5023e
MahabubEmon <mahabubemon.cse@gmail.com>
2025-08-14 00:52:34 +0600

    Add project files.

---
+++ DBFirstDemo/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFirstDemo.Models;
+
+public partial class Student
+{
+    public int Roll { get; set; }
+
+    public string? StuName { get; set; }
+
+    public string? StuGender { get; set; }
+
+    public DateOnly? StuDob { get; set; }
+
+    public string? StuPhone { get; set; }
+}
